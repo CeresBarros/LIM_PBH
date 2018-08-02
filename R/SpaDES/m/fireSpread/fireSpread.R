675b85f76cdda7bfe7ea59bd63541740c5a8b08b
CeresBarros <cbarros@mail.ubc.ca>
2018-08-02 11:34:10 -0500

    crsUsed passed to global params

--- R/SpaDES/m/fireSpread/fireSpread.R
+++ R/SpaDES/m/fireSpread/fireSpread.R
@@ -18,7 +18,7 @@ defineModule(sim, list(
                   "PredictiveEcology/SpaDES.tools@development",
                   "CeresBarros/reproducible@development"),
   parameters = rbind(
-    defineParameter("crsUsed", "character", "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0",
+    defineParameter(".crsUsed", "character", "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0",
                     NA, NA, "CRS to be used. Defaults to the biomassMap projection"),
     defineParameter("fireSize", "integer", 1000L, NA, NA, desc = "Fire size in pixels"),
     defineParameter("vegFeedback", "logical", TRUE, NA, NA, desc = "Should vegetation feedbacks unto fire be simulated? Defaults to TRUE"),
@@ -415,7 +415,7 @@ doNoFire <- function(sim) {
                                                 proj4string = crs(canadaMap))
       
       ## use CRS of biomassMap
-      sim$shpStudyRegionFull <- spTransform(sim$shpStudyRegionFull, CRSobj = P(sim)$crsUsed)
+      sim$shpStudyRegionFull <- spTransform(sim$shpStudyRegionFull, CRSobj = P(sim)$.crsUsed)
       
     } 
     sim$shpStudyRegionFullFBP <- sim$shpStudyRegionFull
