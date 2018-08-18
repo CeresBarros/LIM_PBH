b403ed64f7b1b7e199cf082d4a63c5202522fb5f
Unknown <ceresvbarros@hotmail.com>
2018-08-17 18:59:33 -0700

    Improvements
    * for compatibility across all LandR modules, default inputs are now the same across modules 
    * previous dummies from LANDIS-II were replaced by others with a real geographical location
    * rstCurrentBurn is no longer a list

--- R/SpaDES/m/fireSeverity/fireSeverity.R
+++ R/SpaDES/m/fireSeverity/fireSeverity.R
@@ -14,6 +14,8 @@ defineModule(sim, list(
   documentation = list("README.txt", "fireSeverity.Rmd"),
   reqdPkgs = list("raster"),
   parameters = rbind(
+    defineParameter(".crsUsed", "character", "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0",
+                    NA, NA, "CRS to be used. Defaults to the biomassMap projection"),
     defineParameter(".plotMaps", "logical", FALSE, NA, NA, "This describes whether maps should be plotted or not"),
     defineParameter(name = ".saveInitialTime", class = "numeric", default = 0,
                     min = NA, max = NA, desc = "This describes the simulation time at which the
@@ -22,13 +24,27 @@ defineModule(sim, list(
                     desc = "The number of time units between successive fire events in a fire module")
     ),
   inputObjects = bind_rows(
+    expectsInput(objectName = "pixelGroupMap", objectClass = "RasterLayer",
+                  desc = "updated community map at each succession time step"),
+    expectsInput(objectName = "ecoregionMap", objectClass = "RasterLayer",
+                 desc = "ecoregion map that has mapcodes match ecoregion table and speciesEcoregion table",
+                 sourceURL = ""),
+    expectsInput(objectName = "cohortData", objectClass = "data.table",
+                  desc = "age cohort-biomass table hooked to pixel group map by pixelGroupIndex at
+                  succession time step"),
     expectsInput(objectName = "biomassMap", objectClass = "RasterLayer",
                  desc = "Biomass map at each succession time step"),
     expectsInput(objectName = "biomassMapPreFire", objectClass = "RasterLayer",
                  desc = "Biomass map from before the last fire."),
-    expectsInput(objectName = "rstCurrentBurn", objectClass = "list",
-                 desc = "List of rasters of fire spread"),
-    expectsInput(objectName = "fireYear", objectClass = "numeric", desc =  "Next fire year")
+    expectsInput(objectName = "rstCurrentBurn", objectClass = "RasterLayer",
+                 desc = "Binary raster of fire spread"),
+    expectsInput(objectName = "fireYear", objectClass = "numeric", desc =  "Next fire year"),
+    expectsInput(objectName = "shpStudySubRegion", objectClass = "SpatialPolygonsDataFrame",
+                 desc = "this shape file contains two informaton: Sub study area with fire return interval attribute. 
+                 Defaults to a shapefile in Southwestern Alberta, Canada", sourceURL = ""),
+    expectsInput(objectName = "shpStudyRegionFull", objectClass = "SpatialPolygonsDataFrame",
+                 desc = "this shape file contains two informaton: Full study area with fire return interval attribute.
+                 Defaults to a shapefile in Southwestern Alberta, Canada", sourceURL = "")
   ),
   outputObjects = bind_rows(
     createsOutput(objectName = "severityMap", objectClass = "RasterLayer",
@@ -59,7 +75,7 @@ doEvent.fireSeverity = function(sim, eventTime, eventType, debug = FALSE) {
       }
     },
     calcSeverity = {
-      if(!all(is.na(sim$rstCurrentBurn[[time(sim)]][]))) {
+      if(!all(is.na(sim$rstCurrentBurn[]))) {
         ## calculate severity
         sim <- doSeverity(sim)
         
@@ -69,7 +85,7 @@ doEvent.fireSeverity = function(sim, eventTime, eventType, debug = FALSE) {
       }
     },
     severityPlot = {
-      if(!all(is.na(sim$rstCurrentBurn[[time(sim)]][]))) {
+      if(!all(is.na(sim$rstCurrentBurn[]))) {
         ## Plot severity and vegetation changes
         sim <- doSeverityPlot(sim)
         
@@ -78,7 +94,7 @@ doEvent.fireSeverity = function(sim, eventTime, eventType, debug = FALSE) {
       }
     },
     saveSeverity = {
-      if(!all(is.na(sim$rstCurrentBurn[[time(sim)]][]))) {
+      if(!all(is.na(sim$rstCurrentBurn[]))) {
         ## Plot severity and vegetation changes
         sim <- doSaveSeverity(sim)
         
@@ -101,7 +117,7 @@ fireInit <- function(sim) {
 ### Calculate severity based on vegetation state transitions
 doSeverity <- function(sim){
   ## convert fire spread raster to a mask
-  fireMask <- sim$rstCurrentBurn[[time(sim)]]
+  fireMask <- sim$rstCurrentBurn
   fireMask[!is.na(fireMask)] <- 1
   
   ## Make raster of post-fire biomass  
@@ -150,25 +166,72 @@ doSaveSeverity = function(sim) {
   dPath <- dataPath(sim)
   cacheTags = c(currentModule(sim), "function:.inputObjects")
   
-  if (!suppliedElsewhere("ecoregionMap", sim )) {
-    # load ecoregion map
-    sim$ecoregionMap <- raster(file.path(dPath, "ecoregions.gis"))
+  if (!suppliedElsewhere("shpStudyRegionFull", sim)) {
+    message("'shpStudyRegionFull' was not provided by user. Using a polygon in Southwestern Alberta, Canada")
+    
+    canadaMap <- Cache(getData, 'GADM', country = 'CAN', level = 1, path = asPath(dPath),
+                       cacheRepo = getPaths()$cachePath, quick = FALSE) 
+    smallPolygonCoords = list(coords = data.frame(x = c(-115.9022,-114.9815,-114.3677,-113.4470,-113.5084,-114.4291,-115.3498,-116.4547,-117.1298,-117.3140), 
+                                                  y = c(50.45516,50.45516,50.51654,50.51654,51.62139,52.72624,52.54210,52.48072,52.11243,51.25310)))
+    
+    sim$shpStudyRegionFull <- SpatialPolygons(list(Polygons(list(Polygon(smallPolygonCoords$coords)), ID = "swAB_polygon")),
+                                              proj4string = crs(canadaMap))
+    
+    ## use CRS of biomassMap
+    sim$shpStudyRegionFull <- spTransform(sim$shpStudyRegionFull,
+                                          CRSobj = P(sim)$.crsUsed)
+    
   }
   
-  # if(!suppliedElsewhere("biomassMap", sim)) {
-  #   sim$biomassMap <- Cache(prepInputs,
-  #                           targetFile = biomassMapFilename,
-  #                           archive = asPath(c("kNN-StructureBiomass.tar",
-  #                                              "NFI_MODIS250m_kNN_Structure_Biomass_TotalLiveAboveGround_v0.zip")),
-  #                           url = extractURL("biomassMap", sim), 
-  #                           destinationPath = dPath,
-  #                           studyArea = sim$shpStudySubRegion,
-  #                           useSAcrs = TRUE,
-  #                           method = "bilinear",
-  #                           datatype = "INT2U",
-  #                           filename2 = TRUE,
-  #                           userTags = cacheTags)
-  # } 
+  if (!suppliedElsewhere("shpStudySubRegion", sim)) {
+    message("'shpStudySubRegion' was not provided by user. Using the same as 'shpStudyRegionFull'")
+    sim$shpStudySubRegion <- sim$shpStudyRegionFull
+  }
+  
+  if (!identical(P(sim)$.crsUsed, crs(sim$shpStudyRegionFull))) {
+    sim$shpStudyRegionFull <- spTransform(sim$shpStudyRegionFull, P(sim)$.crsUsed) #faster without Cache
+  }
+  
+  if (!identical(P(sim)$.crsUsed, crs(sim$shpStudySubRegion))) {
+    sim$shpStudySubRegion <- spTransform(sim$shpStudySubRegion, P(sim)$.crsUsed) #faster without Cache
+  }
+  
+  ## load ecoregion map
+  if (!suppliedElsewhere("ecoregionMap", sim )) {
+    ## LANDIS-II demo data:
+    
+    # sim$ecoregionMap <- Cache(prepInputs,
+    #                           url = extractURL("ecoregionMap"),
+    #                           destinationPath = dPath,
+    #                           targetFile = "ecoregions.gis",
+    #                           fun = "raster::raster")
+    
+    ## Dummy version with spatial location in Canada
+    ras <- projectExtent(sim$shpStudySubRegion, crs = sim$shpStudySubRegion)
+    res(ras) = 250
+    ecoregionMap <- rasterize(sim$shpStudySubRegion, ras)
+    
+    ecoregionMap[!is.na(getValues(ecoregionMap))][] <- sample(ecoregion$mapcode, 
+                                                              size = sum(!is.na(getValues(ecoregionMap))), 
+                                                              replace = TRUE) 
+    sim$ecoregionMap <- ecoregionMap
+  }
+  
+  if(!suppliedElsewhere("biomassMap", sim)) {
+    biomassMapFilename <- file.path(dPath, "NFI_MODIS250m_kNN_Structure_Biomass_TotalLiveAboveGround_v0.tif")
+    sim$biomassMap <- Cache(prepInputs,
+                            targetFile = biomassMapFilename,
+                            archive = asPath(c("kNN-StructureBiomass.tar",
+                                               "NFI_MODIS250m_kNN_Structure_Biomass_TotalLiveAboveGround_v0.zip")),
+                            url = extractURL("biomassMap", sim), 
+                            destinationPath = dPath,
+                            studyArea = sim$shpStudySubRegion,
+                            useSAcrs = TRUE,
+                            method = "bilinear",
+                            datatype = "INT2U",
+                            filename2 = TRUE,
+                            userTags = cacheTags)
+  } 
   
   return(invisible(sim))
 }
