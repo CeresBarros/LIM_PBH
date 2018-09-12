f2087513941455a8638cb1f924d7536e44b0090f
CeresBarros <cbarros@mail.ubc.ca>
2018-09-11 21:10:43 -0600

    module updates

--- R/SpaDES/global.R
+++ R/SpaDES/global.R
@@ -14,20 +14,24 @@ rm(list=ls()); gc(reset = TRUE)
 # loading SpaDES.core      0.2.1
 # loading SpaDES.tools     0.3.0
 # loading SpaDES.addins    0.1.1
-# devtools::install_github("CeresBarros/reproducible@development") ## July 29th
+# devtools::install_github("CeresBarros/reproducible@development", force = TRUE)
 # devtools::install_github("PredictiveEcology/quickPlot@development")
-# devtools::install_github("PredictiveEcology/SpaDES.core@development")
-# devtools::install_github("Predictive/SpaDES.tools@development")
+# devtools::install_github("PredictiveEcology/SpaDES.core@development")  ## August 15th
+# devtools::install_github("PredictiveEcology/SpaDES.tools@devel-amc") ## August 27th
 library(SpaDES)
 
 ## testing packages
-# try(detach("package:SpaDES.core", unload = TRUE)); try(detach("package:SpaDES.tools", unload = TRUE)); try(detach("package:reproducible", unload = TRUE)); devtools::load_all("E:/GitHub/reproducible"); devtools::load_all("E:/GitHub/SpaDES.tools"); devtools::load_all("E:/GitHub/SpaDES.core")
+# try(detach("package:SpaDES.core", unload = TRUE))
+# try(detach("package:SpaDES.tools", unload = TRUE))
+# try(detach("package:reproducible", unload = TRUE))
+# devtools::load_all("E:/GitHub/reproducible")
+# devtools::load_all("E:/GitHub/SpaDES.tools")
+# devtools::load_all("E:/GitHub/SpaDES.core")
 
 source("R/R_tools/Useful_functions.R")
 
 ## define paths
-setPaths(cachePath = file.path("R/SpaDES/cache"),
-         modulePath = file.path("R/SpaDES/m"),
+setPaths(modulePath = file.path("R/SpaDES/m"),
          inputPath = file.path("R/SpaDES/inputs"),
          outputPath = file.path("R/SpaDES/outputs"))
 
@@ -45,132 +49,108 @@ speciesList <- as.matrix(read.csv(file.path(getPaths()$inputPath, "speciesList.c
 
 ## simulation parameters
 pathsSim <- getPaths()
-timesSim <- list(start = 1, end = 50)
-
-# modulesSimtoy <- list("simplifyLCCVeg", "simpleLCCSuccession", "fireSpread", "fireSeverity")
-# objectsSimtoy <-  list(studyArea = foothillsSMALL)
-# paramsSimtoy <- list(
-#   .globals = list(.useCache = TRUE),
-#   fireSpread = list(fireSize = 1000, noStartPix = 100, fireFreq = 3),
-#   fireSeverity = list(.plotMaps = TRUE, .plotInterval = 1),
-#   fireStats = list(.plotStats = TRUE)
-# )
-
+timesSim <- list(start = 1, end = 3)
 
 # eventCaching <- c(".inputObjects")
-modulesSimLBMR <- list("BiomassSpeciesData", "Boreal_LBMRDataPrep",
-                       "LandR_BiomassFuels", "fireSpread", "LBMR", "fireSeverity")
-                       
-objectsSimLBMR <- list("shpStudyRegionFull" = foothillsSMALL,
-                       "shpStudySubRegion" = foothillsSMALL,
-                       "speciesList" = speciesList)
+modulesSim <- list("BiomassSpeciesData", "Boreal_LBMRDataPrep",   ## biomassSpeciesData needs a data prep -can't cope with LBMR defaults
+                   "LBMR", "LandR_BiomassGMOrig"#,
+                   # "LandR_BiomassRegen", "LandR_BiomassFuels", "fireSpread", 
+                   #"fireSeverity"
+                   )
+
+objectsSim <- list("shpStudyRegionFull" = foothillsSMALL,
+                   "shpStudySubRegion" = foothillsSMALL#,
+                   # "speciesList" = speciesList
+                   )
+
 outputs <- data.frame(expand.grid(objectName = c("cohortData"),
                                   saveTime = seq(2, 50, by = 5),
                                   stringsAsFactors = FALSE))
 outputs <- rbind(outputs, data.frame(objectName = "rstCurrentBurn", 
                                      saveTime = tail(seq(2, 50, by = 5), 1)))
 
-paramsSimLBMR <- list(
-  # Boreal_LBMRDataPrep = list(crsUsed = projCRS), 
-  # BiomassSpeciesData = list(crsUsed = projCRS),
+paramsSim <- list(
   LBMR = list(successionTimestep = 1,
-              fireTimestep = 5L,
               seedingAlgorithm = "wardDispersal",
               .saveInitialTime = 1,
               .plotInitialTime = timesSim$start#, 
+              # .purge = 7
               # .useCache = eventCaching
-              ),
-  fireSpread = list(fireTimestep = 5L,
-                    vegFeedback = FALSE),
-  fireSeverity = list(fireTimestep = 5L,
-                      .plotMaps = FALSE,
-                      .saveInitialTime = 1)
+              )#,
+  # LandR_BiomassRegen = list(fireTimestep = 2L)#,
+  # fireSpread = list(fireTimestep = 5L,
+  #                   vegFeedback = TRUE)#,
+  # fireSeverity = list(fireTimestep = 5L,
+  #                     .plotMaps = FALSE,
+  #                     .saveInitialTime = 1)
 )
 
-pathsSim$outputPath <- "R/SpaDES/outputs/vegFB_0"
-# pathsSim$outputPath <- file.path(pathsSim$outputPath, "vegFB_1")
+# pathsSim$outputPath <- "R/SpaDES/outputs/vegFB_0"
+pathsSim$outputPath <- file.path(pathsSim$outputPath, "vegFB_1/tests")
+pathsSim$cachePath <- file.path("R/SpaDES/cache/LIM_tests")
 
-showCache(getPaths()$cachePath)
-# clearCache(getPaths()$cachePath#, userTags = "Boreal_LBMRDataPrep"
-# )
-
-LBMR_testSim <- simInit(times = timesSim, params = paramsSimLBMR, modules = modulesSimLBMR,
-                        objects = objectsSimLBMR, outputs = outputs, paths = pathsSim)
+showCache(pathsSim$cachePath, userTags = "simList")
+# reproducible::clearCache(pathsSim$cachePath, userTags = c("simList"))
+LBMR_testSim <- simInit(times = timesSim, params = paramsSim, modules = modulesSim,
+                        objects = objectsSim, outputs = outputs, paths = pathsSim)
 
 graphics.off()
 dev()
 clearPlot()
 LBMR_testSimout <- spades(LBMR_testSim, cache = TRUE, debug = TRUE)   ## debug = TRUE activates automatic browsing when errors occur
-# events(LBMR_testSim)
+
+# events(LBMR_testSimout)
+# year <- 5L
+# LBMR_testSimout <- scheduleEvent(LBMR_testSimout, eventTime = year, eventType = "save", moduleName = "LBMR")
+# events(LBMR_testSimout)
+# 
+# year <- 5.0
+# LBMR_testSimout <- scheduleEvent(LBMR_testSimout, eventTime = year, eventType = "save", moduleName = "LBMR")
+# events(LBMR_testSimout)
+# 
+# year <- time(LBMR_testSimout) + 2
+# LBMR_testSimout <- scheduleEvent(LBMR_testSimout, eventTime = year, eventType = "save", moduleName = "LBMR")
+# events(LBMR_testSimout)
 
 
-paramsSimLBMR <- list(
-  # Boreal_LBMRDataPrep = list(crsUsed = projCRS), 
-  # BiomassSpeciesData = list(crsUsed = projCRS),
-  LBMR = list(successionTimestep = 1,
-              fireTimestep = 5L,
-              seedingAlgorithm = "wardDispersal",
-              .saveInitialTime = 1,
-              .plotInitialTime = timesSim$start#, 
-              # .useCache = eventCaching
-  ),
-  fireSpread = list(fireTimestep = 5L,
-                    vegFeedback = TRUE),
-  fireSeverity = list(fireTimestep = 5L,
-                      .plotMaps = FALSE,
-                      .saveInitialTime = 1)
-)
+## TEST WITH FAKE FIRE MAP
+## make fake fire map
+rstCurrentBurn <- LBMR_testSimout@.envir$pixelGroupMap
+rstCurrentBurn[rstCurrentBurn[]>0] <- 1
+rstCurrentBurn[rstCurrentBurn[] <= 0] <- NA
+IDs <- which(rstCurrentBurn[] == 1)
+rstCurrentBurn[IDs[1:round(length(IDs)/2)]] <- NA
 
-pathsSim$outputPath <- "R/SpaDES/outputs/vegFB_1"
-# pathsSim$outputPath <- file.path(pathsSim$outputPath, "vegFB_1")
+objectsSim["rstCurrentBurn"] <- rstCurrentBurn
 
-showCache(getPaths()$cachePath)
-# clearCache(getPaths()$cachePath#, userTags = "Boreal_LBMRDataPrep"
-# )
+modulesSim <- list("BiomassSpeciesData", "Boreal_LBMRDataPrep",   ## biomassSpeciesData needs a data prep -can't cope with LBMR defaults
+                   "LBMR", "LandR_BiomassGMOrig",
+                   "LandR_BiomassFuels",
+                   "LandR_BiomassRegen", "fireSpread",
+                   "fireSeverity")
 
-LBMR_testSim2 <- simInit(times = timesSim, params = paramsSimLBMR, modules = modulesSimLBMR,
-                        objects = objectsSimLBMR, outputs = outputs, paths = pathsSim)
+LBMR_testSim <- simInit(times = timesSim, params = paramsSim, modules = modulesSim,
+                        objects = objectsSim, outputs = outputs, paths = pathsSim)
 
 graphics.off()
 dev()
 clearPlot()
-LBMR_testSimout2 <- spades(LBMR_testSim2, cache = TRUE, debug = TRUE)   ## debug = TRUE activates automatic browsing when errors occur
-# events(LBMR_testSim)
+LBMR_testSimout <- spades(LBMR_testSim, cache = TRUE, debug = TRUE)   ## debug = TRUE activates automatic browsing when errors occur
+completed(LBMR_testSimout)
 
 
-
-
-
-
-
-# expParams <- list(
-#   # Boreal_LBMRDataPrep = list(crsUsed = projCRS),
-#   # BiomassSpeciesData = list(crsUsed = projCRS),
-#   LBMR = list(successionTimestep = 1,
-#               fireTimestep = 5L,
-#               seedingAlgorithm = "wardDispersal",
-#               .saveInitialTime = 1,
-#               .plotInitialTime = timesSim$start#,
-#               # .useCache = eventCaching
-#   ),
-#   fireSpread = list(fireTimestep = 5L,
-#                     vegFeedback = c(FALSE,TRUE)),
-#   fireSeverity = list(fireTimestep = 5L,
-#                       .plotMaps = FALSE,
-#                       .saveInitialTime = 1)
-# )
+## test species layers in landweb
+# LandWebSA <- raster::shapefile("../LandWeb/inputs/studyarea-correct")
 # 
-# graphics.off()
-# dev()
-# clearPlot()
-# LBMR_experiment <- experiment(LBMR_testSim, replicates = 5, params = expParams,
-#                               experimentFile = TRUE)
-
-## error when veg feeback are true
-# endCluster()
-
-# moduleDiagram(LBMR_testSim)
-# objectDiagram(LBMR_testSim)
-# events(LBMR_testSim)
-
-
+# modulesSim <- list("BiomassSpeciesData")
+# pathsSim$cachePath <- "R/SpaDES/cache/tempLandWeb"
+# paramsSim <- list(BiomassSpeciesData = list(.crsUsed = as.character(raster::crs(LandWebSA))))
+# objectsSim <- list("shpStudyRegionFull" = LandWebSA,
+#                    "shpStudySubRegion" = LandWebSA)
+# 
+# # clearCache(pathsSim$cachePath, userTags = "Pickell")
+# LandWebSpp <- simInit(times = timesSim, params = paramsSim, modules = modulesSim,
+#                         objects = objectsSim, paths = pathsSim)
+# 
+# LandWebSppout <- spades(LandWebSpp, cache = TRUE, debug = TRUE) 
+# 
