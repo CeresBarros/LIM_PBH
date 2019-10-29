951fe41a54c184263de922d9eb110bb446d3852e
Ceres Barros <ceresvbarros@hotmail.com>
2019-10-29 15:27:47 -0700

    minor

--- R/SpaDES/global.R
+++ R/SpaDES/global.R
@@ -49,8 +49,8 @@ source("R/R_tools/Useful_functions.R")
 source("R/SpaDES/1_simObjects.R")
 
 ## Set up modelling parameters  ---------------------------
-runName <- "blogSep2019_PM"
-# runName <- "blogSep2019_noPM"
+# runName <- "blogSep2019_PM"
+runName <- "blogSep2019_noPM"
 eventCaching <- c(".inputObjects", "init")
 useParallel <- FALSE
 
