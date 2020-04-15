45f0fe5138743aaf86734da2f476ef00e06bf354
CeresBarros <cbarros@mail.ubc.ca>
2020-04-14 18:56:52 -0700

    improvements of report plots and new plots

--- R/SpaDES/plotsReport.R
+++ R/SpaDES/plotsReport.R
@@ -1,12 +1,12 @@
 ## ----------------------------------------
 ## PLOTS FOR REPORT
 ## ----------------------------------------
-
 library(qs)
 library(ggplot2)
 library(ggpubr)
 library(ggspatial)
 library(SpaDES)
+library(raster)
 
 ## path to figure folder
 figOutputPath <- "C:/Users/Ceres Barros/Google Drive/Shared/Landscapes In Motion/ModellingTeam/reportFigs/"
@@ -18,11 +18,12 @@ runName <- c("noPM_newSppParams_fullSA", "PM_newSppParams_fullSA")
 simOutPreSim <- qread(file.path("R/SpaDES/outputs/preSim/preSimList.qs"))
 if (length(runName) > 1) {
   for(run in runName) {
-   eval(parse = text(
-    paste0("simOut_", run, " <- qread(R/SpaDES/outputs/",
-         run, "/", paste0("simList_", runName, ".qs"))
+   eval(parse(text =
+    paste0("simOut_", run, " <- qread('R/SpaDES/outputs/",
+         run, "/", paste0("simList_", run, ".qs"), "')")
    ))
   }
+  rm(run)
 }
 
 ## FIRE LOCATIONS -------------------------
@@ -57,22 +58,24 @@ ggsave(file.path(figOutputPath, "fuelsMap.tiff"), plot2,
 
 
 ## FUEL TYPE COVER
-plotFunction <- function(ras) {
+plotFunction <- function(ras, limits) {
   ggplot() +
-    layer_spatial(data = simOutPreSim$studyArea, fill = "grey") +
     layer_spatial(ras, aes(fill = stat(band1))) +
+    layer_spatial(data = simOutPreSim$studyArea, fill = "transparent", colour = "black") +
     annotation_north_arrow(style = north_arrow_minimal,
                            height = unit(1, "cm"), width = unit(1, "cm"),
-                           location = "tr", which_north = "true", ) +
+                           location = "tr", which_north = "true") +
     theme_pubr(legend = "bottom") +
     theme(plot.margin = unit(c(0,0,0,0), units = "mm")) +
     scale_fill_distiller(palette = "Greys", na.value = "transparent",
-                         direction = 1, breaks = seq(0, 1, 0.2),
-                         limits = c(0,1)) +
+                         direction = 1,
+                         breaks = seq(limits[1], limits[2], length.out = 6),
+                         limits = limits) +
     labs(x = "longitude", y = "latitude", fill = "Cover",
-         title = names(ras))
+         title = sub("\\.|_", " ", names(ras)))
 }
-plotList <- lapply(unstack(simOutPreSim$fuelTypesCoverStk), plotFunction)
+plotList <- lapply(unstack(simOutPreSim$fuelTypesCoverStk), plotFunction,
+                   limits = c(0,1))
 plot3 <- ggarrange(plotlist = plotList, ncol = 3, nrow = 3,
           legend = "bottom", common.legend = TRUE)
 
@@ -82,6 +85,93 @@ rm(ras)
 
 
 ## TOPO, VEG, CLIMATE MAPS ----------------
-simOutPreSim$
+## elevation
+if (!inMemory(simOut_noPM_newSppParams_fullSA$DEMRas)) {
+  DEMRas <- sub(".*LandscapesInMotion/", "",
+                filename(simOut_noPM_newSppParams_fullSA$DEMRas))
+  DEMRas <- raster(DEMRas)
+} else
+  DEMRas <- simOut_noPM_newSppParams_fullSA$DEMRas
+
+elevPlot <- ggplot() +
+  layer_spatial(DEMRas, aes(fill = stat(band1))) +
+  layer_spatial(data = simOutPreSim$studyArea, col = "black",
+                fill = "transparent") +
+  annotation_north_arrow(style = north_arrow_minimal,
+                         height = unit(1, "cm"), width = unit(1, "cm"),
+                         location = "tr", which_north = "true") +
+  theme_pubr(legend = "right") +
+  theme(plot.margin = unit(c(0,0,0,0), units = "mm")) +
+  scale_fill_distiller(palette = "Spectral", na.value = "transparent",
+                       direction = -1) +
+  labs(x = "longitude", y = "latitude", fill = "m a.s.l.",
+       title = "Elevation")
+
+## stand biomass (raw)
+if (!inMemory(simOut_noPM_newSppParams_fullSA$rawBiomassMap)) {
+  biomassRas <- sub(".*LandscapesInMotion/", "",
+                filename(simOut_noPM_newSppParams_fullSA$rawBiomassMap))
+  biomassRas <- raster(biomassRas)
+} else
+  biomassRas <- simOut_noPM_newSppParams_fullSA$rawBiomassMap
+
+biomassPlot <- ggplot() +
+  layer_spatial(biomassRas, aes(fill = stat(band1))) +
+  layer_spatial(data = simOutPreSim$studyArea, col = "black",
+                fill = "transparent") +
+  annotation_north_arrow(style = north_arrow_minimal,
+                         height = unit(1, "cm"), width = unit(1, "cm"),
+                         location = "tr", which_north = "true") +
+  theme_pubr(legend = "right") +
+  theme(plot.margin = unit(c(0,0,0,0), units = "mm")) +
+  scale_fill_distiller(palette = "Greens", na.value = "transparent",
+                       direction = 1) +
+  labs(x = "longitude", y = "latitude", fill = expression("g/m"^2),
+       title = "Stand biomass")
+
+## Stand age (pre-corrections)
+if (!inMemory(simOut_noPM_newSppParams_fullSA$standAgeMap)) {
+  ageRas <- sub(".*LandscapesInMotion/", "",
+                    filename(simOut_noPM_newSppParams_fullSA$standAgeMap))
+  ageRas <- raster(ageRas)
+} else
+  ageRas <- simOut_noPM_newSppParams_fullSA$standAgeMap
+
+agePlot <- ggplot() +
+  layer_spatial(ageRas, aes(fill = stat(band1))) +
+  layer_spatial(data = simOutPreSim$studyArea, col = "black",
+                fill = "transparent") +
+  annotation_north_arrow(style = north_arrow_minimal,
+                         height = unit(1, "cm"), width = unit(1, "cm"),
+                         location = "tr", which_north = "true") +
+  theme_pubr(legend = "right") +
+  theme(plot.margin = unit(c(0,0,0,0), units = "mm")) +
+  scale_fill_distiller(palette = "Greens", na.value = "transparent",
+                       direction = 1) +
+  labs(x = "longitude", y = "latitude", fill = "years",
+       title = "Stand age")
+
+plot4 <- ggarrange(elevPlot, agePlot, biomassPlot, ncol = 3, nrow = 1)
+ggsave(file.path(figOutputPath, "elev_StandAge_B.tiff"), plot4,
+       width = 12, height = 7, dpi = 300)
+
 
 ## SPECIES COVER MAPS ---------------------
+if (!inMemory(simOut_noPM_newSppParams_fullSA$speciesLayers)) {
+  sppStk <- unstack(simOut_noPM_newSppParams_fullSA$speciesLayers)
+  sppStk <- sub(".*LandscapesInMotion/", "",
+                sapply(sppStk, filename))
+  sppStk <- stack(sppStk)
+  names(sppStk) <- sub(".*\\.", "", sub(".tif", "", names(sppStk)))
+} else
+  sppStk <- simOut_noPM_newSppParams_fullSA$speciesLayers
+
+names(sppStk) <- LandR::equivalentName(value = names(sppStk), column = "EN_generic_full",
+                                       df = simOutPreSim$sppEquiv)
+
+plotList <- lapply(unstack(sppStk), plotFunction, limits = c(0, 100))
+plot5 <- ggarrange(plotlist = plotList, ncol = 3, nrow = 2,
+                     legend = "bottom", common.legend = TRUE)
+
+ggsave(file.path(figOutputPath, "Species_cover.tiff"), plot5,
+       width = 9, height = 10, dpi = 300)
