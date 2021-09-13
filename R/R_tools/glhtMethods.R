1dde62b74e1cdef3cf1fe40173837bc59510bb48
CeresBarros <cbarros@mail.ubc.ca>
2021-09-13 14:01:10 -0700

    gls methods for glht

---
+++ R/R_tools/glhtMethods.R
@@ -0,0 +1,12 @@
+## GLS METHODS FOR glht
+
+model.matrix.gls <- function(object, ...)
+  model.matrix(terms(object), data = getData(object), ...)
+
+
+model.frame.gls <- function(object, ...)
+  model.frame(formula(object), data = getData(object), ...)
+
+
+terms.gls <- function(object, ...)
+  terms(model.frame(object),...)
