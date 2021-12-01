f80d62d9d2f4ccafc083694618c4e57e95fc7a71
CeresBarros <cbarros@mail.ubc.ca>
2021-12-01 15:50:15 -0800

    count/estimate no parameters for simulation AIC

---
+++ R/SpaDES/parameter_inputs_diff_Scens.R
@@ -0,0 +1,32 @@
+## --------------------------------------------------
+##  FINDING DIFFERENCES IN INPUTS AND PARAMETERS
+##  bETWEEN SCENARIOS
+## --------------------------------------------------
+
+library(SpaDES)
+
+options("reproducible.useNewDigestAlgorithm" = 2)
+options("spades.moduleCodeChecks" = FALSE)
+options("reproducible.useCache" = TRUE)
+options("reproducible.destinationPath" = normPath("R/SpaDES/inputs"))
+options("reproducible.useGDAL" = FALSE)
+
+
+## general paths
+simDirName <- "jun2021Runs"
+simPaths <- list(cachePath = file.path("R/SpaDES/cache", simDirName, "postSimAnalyses")
+                 , modulePath = file.path("R/SpaDES/m")
+                 , inputPath = file.path("R/SpaDES/inputs")
+                 , outputPath = file.path("R/SpaDES/outputs", simDirName))
+
+preSimListnoPM <- loadSimList(file.path(simPaths$outputPath, "noPM", "LIM_simInit_noPM"))
+preSimListPM <- loadSimList(file.path(simPaths$outputPath, "PM", "LIM_simInit_PM"))
+
+paramsnoPM <- params(preSimList)$Biomass_regeneration
+paramsPM <- params(preSimList)$Biomass_regenerationPM
+
+inputsnoPM <- depends(preSimListnoPM)@dependencies$Biomass_regeneration@inputObjects
+inputsPM <- depends(preSimListPM)@dependencies$Biomass_regenerationPM@inputObjects
+
+diffInputsnoPM <- setdiff(inputsnoPM$objectName, inputsPM$objectName)
+diffInputsPM <- setdiff(inputsPM$objectName, inputsnoPM$objectName)
