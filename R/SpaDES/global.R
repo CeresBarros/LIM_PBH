d62ceaeb782679464bb9822f930ca36bcd03bc48
CeresBarros <cbarros@mail.ubc.ca>
2023-08-25 10:44:23 -0600

    options for rstudio and root user

--- R/SpaDES/global.R
+++ R/SpaDES/global.R
@@ -7,7 +7,7 @@
 
 if (!exists("pkgDir")) {
   pkgDir <- file.path(
-    if (Sys.info()[["user"]] == "rstudio") "packages_docker" else "packages",
+    if (Sys.info()[["user"]] %in% c("rstudio", "root")) "packages_docker" else "packages",
     version$platform,
     paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1])
   )
