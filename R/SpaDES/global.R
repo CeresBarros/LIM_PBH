f112d0d1b7069ae01b9255d3578d40941c316121
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2025-07-09 14:15:11 -0700

    mv pkg installation to sourced script

--- R/SpaDES/global.R
+++ R/SpaDES/global.R
@@ -1,53 +1,21 @@
 ## ------------------------------------------------------
-## FIRE MODELLING WITH SpaDES
-## TESTS
+## FIRE MODELLING WITH SpaDES - global simulation scrip
 ##
-## Ceres: Nov 2017
+## Ceres: Nov 2017, last updated: July 2025
 ## ------------------------------------------------------
 
-if (!exists("pkgDir")) {
-  pkgDir <- file.path(
-    if (Sys.info()[["user"]] %in% c("rstudio", "root")) "packages_docker" else "packages",
-    version$platform,
-    paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1])
-  )
+source("R/SpaDES/0_packages.R")
 
-  if (!dir.exists(pkgDir)) {
-    dir.create(pkgDir, recursive = TRUE)
-  }
-  .libPaths(pkgDir)
-}
-
-if (!require("Require")) {
-  remotes::install_github("PredictiveEcology/Require@7eaa3af6443fa9acf8ef461d9a02e544174eda38", upgrade = FALSE)
-}
-
-if (FALSE) {
-  ## write file
-  # Require::pkgSnapshot("packages_docker/pkgSnapshot.txt", standAlone = TRUE, exact = TRUE)
-  # Much later on a different or same machine
-  Require::Require(packageVersionFile = "packages_docker/pkgSnapshot.txt", standAlone = TRUE)
-
-  ## these versions need to be ensured and if not on the snapshot file
-  devtools::install_github("PredictiveEcology/reproducible@97f147033f10061dfe40da4ce76575dfef89dfbd", upgrade = FALSE)
-  devtools::install_github("CeresBarros/SpaDES.core@2e8b95b04cf6b93bc45796ddc8a55c8d1618432d", upgrade = FALSE)
-  devtools::install_github("CeresBarros/SpaDES.tools@780cd50cbf156faa86d7cb3a609c6d5edf359752", upgrade = FALSE)
-  devtools::install_github("ianmseddy/PSPclean@3c3f0e7082e14c111a607c3ba803abf0396343e6", upgrade = FALSE)
-  devtools::install_github("CeresBarros/SpaDES.experiment@82ffdb7cce912013e19a49d70da2572448605250", upgrade = FALSE)
-  devtools::install_github("PredictiveEcology/LandR@a2df2a33fcde78ee16f73565102716fa58917d8b", upgrade = FALSE)
-  devtools::install_github("CeresBarros/ToolsCB@8cdcc5494fdb48c3a3df47c93b2d2cc65c21ce96", upgrade = FALSE)
-}
-
-Require::Require(c("raster",
-                   "SpaDES",
-                   "data.table",
-                   "ToolsCB",
-                   "SpaDES.experiment",
-                   "LandR",
-                   "reproducible",
-                   "future"
-),
-upgrade = FALSE, install = FALSE)
+library(Require)
+Require(c("raster",
+          "SpaDES",
+          "data.table",
+          "ToolsCB",
+          "SpaDES.experiment",
+          "LandR",
+          "reproducible",
+          "future"
+), upgrade = FALSE, install = FALSE)
 
 ## to prevent overflow to threads that aren't actually available
 data.table::setDTthreads(threads = 1)
@@ -225,11 +193,11 @@ plan("multicore", workers = 2)   ##
 
 clearSimEnv <- TRUE
 simExperimentOut <- experiment2(#noPM = LIM_simInitList[["noPM"]],
-                                PM = LIM_simInitList[["PM"]],
-                                clearSimEnv = clearSimEnv,
-                                replicates = 5,
-                                useCache = FALSE,
-                                meanStaggerIntervalInSecs = 60)
+  PM = LIM_simInitList[["PM"]],
+  clearSimEnv = clearSimEnv,
+  replicates = 5,
+  useCache = FALSE,
+  meanStaggerIntervalInSecs = 60)
 future:::ClusterRegistry("stop")
 
 ## save simLists object.
