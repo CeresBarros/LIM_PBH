894a0b9caccb6229bf1c5b12d9d007c2ef5eba1d
CeresBarros <ceres.barros@nrcan-rncan.gc.ca>
2026-03-13 23:56:53 +0000

    add Cache

--- R/R_tools/prepFireData4HVs.R
+++ R/R_tools/prepFireData4HVs.R
@@ -2,5 +2,9 @@
 ## summarize data first - as in Steel et al 2021, fire properties are summarized across time, but by pixel
 ## (and by scenario/rep)
 ## only look at pixels with vegetation dynamics so that we can compare with biodiv. HVs
-
-summaryFireAttributes <- summariseFireRegAttrs(allPixelBurnData, allPixelCohortDataMnt)
+cacheExtra <- c(digest::digest(allPixelBurnData), digest::digest(allPixelCohortDataMnt))
+summaryFireAttributes <- Cache(summariseFireRegAttrs,
+                               allPixelBurnData = allPixelBurnData,
+                               allPixelCohortDataMnt = allPixelCohortDataMnt,
+                               omitArgs = c("allPixelBurnData", "allPixelCohortDataMnt"),
+                               .cacheExtra = cacheExtra)
